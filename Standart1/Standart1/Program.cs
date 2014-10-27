9e8baffda2af57ed11cecf264e2f7693a4dd56b4
Djimon <Djimon@gmx.de>
2014-10-28 00:55:24 +0100

    standartinitial 1

---
+++ Standart1/Standart1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Standart1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            RenderWindow Startscreen = new RenderWindow(new VideoMode(1024, 768), "Window1");
+            while (Window.IsOpen) ;
+            Window.Closed += WindowClosed;
+          
+
+        }
+
+
+
+        public static EventHandler WindowClosed { object sender; EventArgs e; }
+        ((RenderWindow) sender).close();
+        Window.DispatchEvent(); 
+    }
+}
